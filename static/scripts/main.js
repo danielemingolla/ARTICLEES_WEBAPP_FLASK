2e58c9931b7d7e291899343ec362e2706f993af2
Daniele Mingolla <33353183+danielemingolla@users.noreply.github.com>
2019-08-28 22:47:02 +0200

    some fix for mobile devices
    Fixed text in card-text and added different text length for different devices to improve the user experience

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,13 +1,26 @@
-var MAXCHARACTERS = 512;
+var MAXCHARACTERS = 600;
 
 $(".card-text").each(function(i) {
   oldText = $(this).text();
-  console.log("OLD: " + oldText);
+  windowSize = window.innerWidth;
+  //IPHONE 5/SE
+  if (windowSize<321) {
+    MAXCHARACTERS = 420;
+  }
+  //IPAD
+  else if (windowSize >= 768 && windowSize<1020){
+    MAXCHARACTERS = 300;
+  }
+  //IPAD PRO
+  else if(windowSize < 1900){
+    MAXCHARACTERS = 560;
+  }
+  //MY BROWSER SIZE
+  else{
+    MAXCHARACTERS = 650;
+  }
   if (oldText.length > MAXCHARACTERS) {
     newText = oldText.substring(0, MAXCHARACTERS);
     $(this).text(newText + " [...]");
-    console.log("NEW: " + newText);
   }
 });
-
-
