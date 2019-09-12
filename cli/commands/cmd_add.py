5b71d719a2d3dc04312558d98930d661a3984b0a
Daniele Mingolla <33353183+danielemingolla@users.noreply.github.com>
2019-09-12 22:21:21 +0200

    Update cmd_add.py

--- cli/commands/cmd_add.py
+++ cli/commands/cmd_add.py
@@ -14,11 +14,12 @@ fake = Faker()
 
 # article add <--p-articles/--p-users> <--count= INTEGER>
 
+
 @click.command()
 @click.option('--p-articles/--p-users', default=True, help='Populate Users o Article?')
-@click.option('--count',default=1, type=click.IntRange(min=1), help='Number of row to generate.')
+@click.option('--count', default=1, type=click.IntRange(min=1), help='Number of row to generate.')
 @click.argument('path', default='articlee')
-def cli(p_articles,count,path):
+def cli(p_articles, count, path):
     """
     Generate fake data to populate database.
     """
@@ -35,9 +36,11 @@ def cli(p_articles,count,path):
             # Scelto di un username random tra quelli degli utenti registrati
             random_author = Users.query.filter(
                 Users.id == random.randint(1, Users.query.count())).first().username
-                
-            fake_body = str(fake.texts(nb_texts=2, max_nb_chars=100, ext_word_list=None))
-            article = Articles(title=fake_title, author=random_author, body=fake_body)
+
+            fake_body = str(fake.texts(
+                nb_texts=2, max_nb_chars=100, ext_word_list=None))
+            article = Articles(
+                title=fake_title, author=random_author, body=fake_body)
             with app.app_context():
                 db.session.add(article)
                 db.session.commit()
@@ -53,9 +56,10 @@ def cli(p_articles,count,path):
             fake_password = sha256_crypt.hash(str("daniele"))
             fake_email = fake.email()
             fake_username = fake.first_name()
-            user = Users(name=fake_name,password=fake_password,email=fake_email,username=fake_username)
+            user = Users(name=fake_name, password=fake_password,
+                         email=fake_email, username=fake_username)
             with app.app_context():
                 db.session.add(user)
                 db.session.commit()
-    click.echo("I've finish!")
+    click.echo("I've finish. Rows generated: %d" % count)
     return None
